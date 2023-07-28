79a3b2a3ae4820ef86e703e35955159c425cb984
Francis Yang <fy50167@gmail.com>
2023-07-27 20:59:08 -0700

    Finishing best sellers UI

--- assets/js/bookshelf.js
+++ assets/js/bookshelf.js
@@ -19,16 +19,15 @@ for (let i = 0; i < myBookshelf.length; i++) {
     let card = document.createElement('div');
     card.classList.add('rounded-xl', 'bg-gray-200', 'card-width', 'flex', 'flex-col', 'p-4', 'justify-evenly', 'my-4', 'mr-auto');
     card.innerHTML += `<div class = 'flex w-full card-top-height'>
-                        <a target = '_blank' href = '${book_url}' class = 'card-img rounded-xl'>
-                            <img src = '${img_url}' class = 'w-full h-full m-0 rounded-xl'>
-                        </a>
+        
+                            <img src = '${img_url}' class = 'card-img rounded-xl m-0 rounded-xl'>               
                         <div class = 'flex flex-col pl-4 grow'>
                             <h2 class = 'font-bold text-lg'>Title</h2>
-                            <h3 class = 'font-normal text-base'>${title}</h3>
+                            <h3 class = 'book-title font-normal text-base'>${title}</h3>
                             <h2 class = 'font-bold text-lg'>Price</h2>
                             <h3 class = 'font-normal text-base'>$${price}</h3>
                             <h2 class = 'font-bold text-lg'>Author</h2>
-                            <h3 class = 'font-normal text-base'>${author}</h3>
+                            <h3 class = 'font-normal text-base author'>${author}</h3>
                         </div>
                         </div>
 
@@ -52,6 +51,29 @@ for (let i = 0; i < myBookshelf.length; i++) {
 function paintBookshelf() {
 
 }
+
+const bookTitles = document.querySelectorAll('.book-title');
+console.log(bookTitles);
+
+  bookTitles.forEach(bookTitle => {
+    bookTitle.addEventListener('click', function(event) {
+      event.preventDefault();
+  
+      // Get the book title from the text content of the clicked element
+      const bookTitle = this.textContent;
+      console.log(bookTitle);
+  
+      const parentDiv = this.closest('div');
+    
+      // Get the author from the sibling <p> element of the book title
+      const author = parentDiv.querySelector('.author').textContent;
+
+      const pagegenerate = bookTitle + author;
+  
+      // Generate the link
+      window.location.href = `book-details.html?title=${encodeURIComponent(pagegenerate)}`;
+    });
+  });
 
 
 
