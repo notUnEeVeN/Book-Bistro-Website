9cf2663a7a14657a86fdcb4017d79b5da0a47591
notUnEeVeN <tybalt.mallet@gmail.com>
2023-07-31 19:12:21 -0700

    re organized the code using the prettier software for a more organized aesthetic

--- assets/js/book-details.js
+++ assets/js/book-details.js
@@ -1,41 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const urlParams = new URLSearchParams(window.location.search);
-  const bookTitle = urlParams.get('title');
-  
-  const apiURL = `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(bookTitle)}`;
-  
+  const bookTitle = urlParams.get("title");
+
+  const apiURL = `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(
+    bookTitle
+  )}`;
+
   // Variable to store the text snippet
   let bookSnippet;
 
   fetch(apiURL)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       console.log(data);
       if (data.items && data.items.length > 0) {
         const book = data.items[0].volumeInfo;
         bookSnippet = data.items[0].searchInfo.textSnippet; // Store the text snippet
 
-        document.getElementById('book-title').innerText = book.title;
-        document.getElementById('book-author').innerText = book.authors.join(', ');
-        document.getElementById('book-description').innerText = book.description;
-        document.getElementById('book-publisher').innerText = book.publisher;
-        document.getElementById('book-publish-date').innerText = book.publishedDate;
-        document.getElementById('book-isbn').innerText = book.industryIdentifiers[0].identifier;
-        document.getElementById('book-cover').src = book.imageLinks.thumbnail;
-        document.getElementById('book-category').innerText = book.categories.join(', '); // Add this line to display book category
+        document.getElementById("book-title").innerText = book.title;
+        document.getElementById("book-author").innerText =
+          book.authors.join(", ");
+        document.getElementById("book-description").innerText =
+          book.description;
+        document.getElementById("book-publisher").innerText = book.publisher;
+        document.getElementById("book-publish-date").innerText =
+          book.publishedDate;
+        document.getElementById("book-isbn").innerText =
+          book.industryIdentifiers[0].identifier;
+        document.getElementById("book-cover").src = book.imageLinks.thumbnail;
+        document.getElementById("book-category").innerText =
+          book.categories.join(", "); // Add this line to display book category
       }
     })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 
-  const addToBookshelfButton = document.querySelector('.add-to-bookshelf');
+  const addToBookshelfButton = document.querySelector(".add-to-bookshelf");
 
-  addToBookshelfButton.addEventListener('click', function(event) {
+  addToBookshelfButton.addEventListener("click", function (event) {
     event.preventDefault();
-    
-    const bookTitle = document.querySelector('#book-title').textContent;
-    const bookAuthor = document.querySelector('#book-author').textContent;
-    const bookCover = document.querySelector('#book-cover').src;
-    const bookCategory = document.querySelector('#book-category').textContent;
+
+    const bookTitle = document.querySelector("#book-title").textContent;
+    const bookAuthor = document.querySelector("#book-author").textContent;
+    const bookCover = document.querySelector("#book-cover").src;
+    const bookCategory = document.querySelector("#book-category").textContent;
 
     // Construct the book object
     const book = {
@@ -43,35 +50,41 @@ document.addEventListener("DOMContentLoaded", function() {
       author: bookAuthor,
       description: bookSnippet, // Use the bookSnippet here
       book_image: bookCover,
-      category: bookCategory
+      category: bookCategory,
     };
 
-    let myBookshelf = JSON.parse(localStorage.getItem('myBookshelf')) || [];
+    let myBookshelf = JSON.parse(localStorage.getItem("myBookshelf")) || [];
     myBookshelf.push(book);
-    localStorage.setItem('myBookshelf', JSON.stringify(myBookshelf));
-    window.location.href = 'saved-books.html';
+    localStorage.setItem("myBookshelf", JSON.stringify(myBookshelf));
+    window.location.href = "saved-books.html";
   });
 });
 
-const logoEl = document.getElementById('logo');
+const logoEl = document.getElementById("logo");
 
-logoEl.addEventListener('click', function() {
-  location.replace('index.html');
-})
+logoEl.addEventListener("click", function () {
+  location.replace("index.html");
+});
 
+const searchInput = document.querySelector(".searchbar");
 
-const searchInput = document.querySelector('.searchbar');
+searchInput.addEventListener("keypress", function (event) {
+  // Check if the Enter key is pressed (key code 13)
+  if (event.key === "Enter") {
+    // Get the value of the search bar input
+    const searchQuery = searchInput.value;
 
-  searchInput.addEventListener('keypress', function(event) {
-    // Check if the Enter key is pressed (key code 13)
-    if (event.key === 'Enter') {
-      // Get the value of the search bar input
-      const searchQuery = searchInput.value;
+    // Construct the URL for the book-details.html page with the search query as a parameter
+    const url = `book-details.html?title=${encodeURIComponent(searchQuery)}`;
 
-      // Construct the URL for the book-details.html page with the search query as a parameter
-      const url = `book-details.html?title=${encodeURIComponent(searchQuery)}`;
+    // Redirect the user to the book-details.html page with the constructed URL
+    window.location.href = url;
+  }
+});
 
-      // Redirect the user to the book-details.html page with the constructed URL
-      window.location.href = url;
-    }
-  });
+var navToggle = document.getElementById("nav-toggle");
+var dropdownNavContent = document.getElementById("dropdown-nav-content");
+
+navToggle.addEventListener("click", function () {
+  dropdownNavContent.classList.toggle("hidden");
+});
--- assets/js/bookshelf.js
+++ assets/js/bookshelf.js
@@ -1,24 +1,34 @@
-const bookshelfEl = document.getElementById('bookshelf-section')
-const logoEl = document.getElementById('logo');
+const bookshelfEl = document.getElementById("bookshelf-section");
+const logoEl = document.getElementById("logo");
 let title; // Initializing variables to avoid having to re-initialize them for every iteration of later loop; not sure if this is best practice?
 let price;
 let author;
 let description;
 let img_url;
 let book_url;
-let myBookshelf = JSON.parse(localStorage.getItem('myBookshelf')); // Get data from local storage.
+let myBookshelf = JSON.parse(localStorage.getItem("myBookshelf")); // Get data from local storage.
 
 for (let i = 0; i < myBookshelf.length; i++) {
-    title = myBookshelf[i].title;
-    price = myBookshelf[i].price;
-    author = myBookshelf[i].author;
-    description = myBookshelf[i].description;
-    description = truncateString(description, 400); // Truncating description and adding ellipses after if it exceeds a certain length to ensure that cards don't get too big.
-    img_url = myBookshelf[i].book_image;
-    book_url = myBookshelf[i].amazon_product_url;
-    let card = document.createElement('div');
-    card.classList.add('rounded-xl', 'bg-gray-200', 'card-width', 'flex', 'flex-col', 'p-4', 'justify-evenly', 'my-4', 'mr-auto');
-    card.innerHTML += `<div class = 'flex w-full card-top-height'>
+  title = myBookshelf[i].title;
+  price = myBookshelf[i].price;
+  author = myBookshelf[i].author;
+  description = myBookshelf[i].description;
+  description = truncateString(description, 400); // Truncating description and adding ellipses after if it exceeds a certain length to ensure that cards don't get too big.
+  img_url = myBookshelf[i].book_image;
+  book_url = myBookshelf[i].amazon_product_url;
+  let card = document.createElement("div");
+  card.classList.add(
+    "rounded-xl",
+    "bg-gray-200",
+    "card-width",
+    "flex",
+    "flex-col",
+    "p-4",
+    "justify-evenly",
+    "my-4",
+    "mr-auto"
+  );
+  card.innerHTML += `<div class = 'flex w-full card-top-height'>
         
                             <img src = '${img_url}' class = 'card-img rounded-xl m-0 rounded-xl'>               
                         <div class = 'flex flex-col pl-4 grow'>
@@ -37,68 +47,63 @@ for (let i = 0; i < myBookshelf.length; i++) {
                         </p>
                         </div>
                         <a target = '_blank' href = '${book_url}'><h3 class = 'font-bold'>Read more >>></h3></a>
-                        <h3 class = 'font-bold remove-btn'>Remove 🗑️</h3>`
-    let remove = card.getElementsByTagName('h3')[4];
-    remove.addEventListener('click', function() {
-        bookshelfEl.children[i].remove();
-        myBookshelf.splice(i, 1);
-        localStorage.setItem('myBookshelf', JSON.stringify(myBookshelf));
-        
-    })
-    bookshelfEl.append(card);
-}
-
-function paintBookshelf() {
-
-}
-
-const bookTitles = document.querySelectorAll('.book-title');
-
-  bookTitles.forEach(bookTitle => {
-    bookTitle.addEventListener('click', function(event) {
-      event.preventDefault();
-  
-      // Get the book title from the text content of the clicked element
-      const bookTitle = this.textContent;
-      console.log(bookTitle);
-  
-      const parentDiv = this.closest('div');
-    
-      // Get the author from the sibling <p> element of the book title
-      const author = parentDiv.querySelector('.author').textContent;
-
-      const pagegenerate = bookTitle + author;
-  
-      // Generate the link
-      window.location.href = `book-details.html?title=${encodeURIComponent(pagegenerate)}`;
-    });
+                        <h3 class = 'font-bold remove-btn'>Remove 🗑️</h3>`;
+  let remove = card.getElementsByTagName("h3")[4];
+  remove.addEventListener("click", function () {
+    bookshelfEl.children[i].remove();
+    myBookshelf.splice(i, 1);
+    localStorage.setItem("myBookshelf", JSON.stringify(myBookshelf));
   });
+  bookshelfEl.append(card);
+}
 
+function paintBookshelf() {}
 
+const bookTitles = document.querySelectorAll(".book-title");
 
+bookTitles.forEach((bookTitle) => {
+  bookTitle.addEventListener("click", function (event) {
+    event.preventDefault();
 
+    // Get the book title from the text content of the clicked element
+    const bookTitle = this.textContent;
+    console.log(bookTitle);
+
+    const parentDiv = this.closest("div");
+
+    // Get the author from the sibling <p> element of the book title
+    const author = parentDiv.querySelector(".author").textContent;
+
+    const pagegenerate = bookTitle + author;
+
+    // Generate the link
+    window.location.href = `book-details.html?title=${encodeURIComponent(
+      pagegenerate
+    )}`;
+  });
+});
 
 function truncateString(str, length) {
-	if (str.length > length) return str.slice(0, length) + '...';
-	return str;
-};
+  if (str.length > length) return str.slice(0, length) + "...";
+  return str;
+}
 
-logoEl.addEventListener('click', function() {
-    location.replace('index.html');
-})
+logoEl.addEventListener("click", function () {
+  location.replace("index.html");
+});
 
-const searchInput = document.querySelector('.searchbar');
+const searchInput = document.querySelector(".searchbar");
 
-  searchInput.addEventListener('keypress', function(event) {
-    // Check if the Enter key is pressed (key code 13)
-    if (event.key === 'Enter') {
-      // Get the value of the search bar input
-      const searchQuery = searchInput.value;
+searchInput.addEventListener("keypress", function (event) {
+  // Check if the Enter key is pressed (key code 13)
+  if (event.key === "Enter") {
+    // Get the value of the search bar input
+    const searchQuery = searchInput.value;
 
-      // Construct the URL for the book-details.html page with the search query as a parameter
-      const url = `book-details.html?title=${encodeURIComponent(searchQuery)}`;
+    // Construct the URL for the book-details.html page with the search query as a parameter
+    const url = `book-details.html?title=${encodeURIComponent(searchQuery)}`;
 
-      // Redirect the user to the book-details.html page with the constructed URL
-      window.location.href = url;
-    }
-  });
+    // Redirect the user to the book-details.html page with the constructed URL
+    window.location.href = url;
+  }
+});
